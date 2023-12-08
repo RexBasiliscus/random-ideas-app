77f9016fb95cb3ed290d9f5e036c916d467ef020
RexBasiliscus <alexander-219@outlook.com>
2023-12-08 13:25:56 +0100

    Created PUT, DELETE and POST requests to the server.

--- routes/ideas.js
+++ routes/ideas.js
@@ -42,4 +42,48 @@ router.get('/:id', (req, res) => {
   res.json({ success: true, data: idea });
 });
 
+// add an idea
+router.post('/', (req, res) => {
+  const idea = {
+    id: ideas.length + 1,
+    text: req.body.text,
+    tag: req.body.tag,
+    username: req.body.username,
+    date: new Date().toISOString().slice(0, 10),
+  }
+
+  ideas.push(idea);
+
+  res.send({ success: true, data: idea });
+});
+
+// update idea
+router.put('/:id', (req, res) => {
+  const idea = ideas.find((idea) => idea.id === +req.params.id); 
+  // + added to make it into a number, because IDs are numbers
+
+  if (!idea) {
+    return res.status(404).json({ success: false, error: 'Resource not fund' });
+  }
+
+  idea.text = req.body.text || idea.text;
+  idea.tag = req.body.tag || idea.tag;
+
+  res.json({ success: true, data: idea });
+});
+
+// delete an idea
+router.delete('/:id', (req, res) => {
+  const idea = ideas.find((idea) => idea.id === +req.params.id);
+
+  if (!idea) {
+    return res.status(404).json({ success: false, error: 'Resource not fund' });
+  }
+
+  const index = ideas.indexOf(idea);
+  ideas.splice(index, 1);
+
+  res.json({ success: true, data: {} });
+})
+
 module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const port = 5000;
 
 const app = express();
 
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
   res.send({ message:'Welcome to the RandomIdeas API' });
 });
@@ -11,3 +15,5 @@ const ideasRouter = require('./routes/ideas');
 app.use('/api/ideas', ideasRouter);
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
+
+
